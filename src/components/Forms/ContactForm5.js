79915e01e0c9a546b2f87ce0f5598bcac703217a
Jejo <jejomardorongon@gmail.com>
2021-02-07 19:31:30 +0800

    Developed contact form

--- src/components/Forms/ContactForm5.js
+++ src/components/Forms/ContactForm5.js
@@ -1,8 +1,12 @@
-import React from "react"
+import React, { useState } from "react"
 import { useForm, Controller } from "react-hook-form"
 import axios from "axios"
 import ReactDatePicker from "react-datepicker"
-import ReactSelect from "react-select"
+import ReactSelect, { components } from "react-select"
+import SearchIcon from "../../images/search.svg"
+import ChevronDown from "../../images/arrow-down.svg"
+import Calendar from "../../images/calendar.svg"
+import Loading from "../../images/loading.svg"
 import "react-datepicker/dist/react-datepicker.css"
 import suburbs from "../../data/suburbs"
 
@@ -16,6 +20,10 @@ const ContactForm5 = () => {
     watch,
   } = useForm()
 
+  const [isFormSubmitting, setFormSubmit] = useState(false)
+
+  const [isFormSubmitted, setFormSubmitted] = useState(false)
+
   const deliveryDate = watch("deliveryDate")
   const returnDate = watch("returnDate")
 
@@ -26,18 +34,27 @@ const ContactForm5 = () => {
   //declare options for suburb
   let options = []
 
-  suburbs.map(item => {
+  suburbs.forEach(item => {
     options.push({
       value: item,
       label: item,
     })
   })
 
+  //change dropdown indicator
+  const DropdownIndicator = props => {
+    return (
+      <components.DropdownIndicator {...props}>
+        <SearchIcon />
+      </components.DropdownIndicator>
+    )
+  }
+
   //styles for react-select
   const customStyles = {
     control: (base, state) => ({
       ...base,
-      padding: "0.5rem 1rem",
+      padding: "4px 1rem",
       borderRadius: "2px",
       boxShadow: state.isFocused ? "0 0 0 0.1rem #182B33" : 0,
       borderColor: state.isFocused ? "#182B33" : "#182b3347",
@@ -57,7 +74,7 @@ const ContactForm5 = () => {
   }
 
   const onSubmit = data => {
-    //console.log(data)
+    setFormSubmit(true)
     let bodyFormData = new FormData()
 
     bodyFormData.append("your-name", data.name)
@@ -74,211 +91,247 @@ const ContactForm5 = () => {
       .then(res => {
         if (res.data.status === "mail_sent") {
           console.log("Mail sent!")
+          setFormSubmit(false)
+          setFormSubmitted(true)
         }
       })
       .catch(err => {
         console.log(err)
       })
   }
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="cs-form">
-      <div className="name-email">
-        <div>
-          <label htmlFor="name">Name</label>
-          <div>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Name"
-              className={`${errors.name ? "ring-2 ring-red-500" : ""}`}
-              ref={register({
-                required: "Name is required",
-                maxLength: {
-                  value: 80,
-                  message: "Name should not be more than 80 characters",
-                },
-                minLength: {
-                  value: 2,
-                  message: "Name must be at least 2 characters.",
-                },
-              })}
-              required
-            />
-            {errors.name && errors.name.message && <p>{errors.name.message}</p>}
-          </div>
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <div>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Email"
-              className={`${errors.email ? "ring-2 ring-red-500" : ""}`}
-              ref={register({
-                required: "Email is required",
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                  message: "Invalid email address.",
-                },
-              })}
-              required
-            />
-            {errors.email && errors.email.message && (
-              <p>{errors.email.message}</p>
-            )}
-          </div>
-        </div>
-      </div>
-      <div className="suburbs">
-        <div>
-          <label htmlFor="suburb">Suburbs</label>
-          <div>
-            <Controller
-              name="suburb"
-              id="suburb"
-              control={control}
-              rules={{ required: "Please select your suburb." }}
-              defaultValue=""
-              render={({ onChange }) => (
-                <ReactSelect
-                  onChange={onChange}
-                  options={options}
-                  isClearable
-                  isSearchable
-                  styles={customStyles}
-                  placeholder="Type your suburb name..."
-                  className={`${errors.suburb ? "ring-2 ring-red-500" : ""}`}
+    <>
+      {!isFormSubmitted && (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="cs-form mt-4 relative"
+        >
+          <div className="name-email cs-form-control">
+            <div>
+              <label htmlFor="name">Name</label>
+              <div>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  className={`${errors.name ? "ring-2 ring-red-500" : ""}`}
+                  ref={register({
+                    required: "Name is required",
+                    maxLength: {
+                      value: 80,
+                      message: "Name should not be more than 80 characters",
+                    },
+                    minLength: {
+                      value: 2,
+                      message: "Name must be at least 2 characters.",
+                    },
+                  })}
+                  required
                 />
-              )}
-            />
-            {errors.suburb && errors.suburb.message && (
-              <p>{errors.suburb.message}</p>
-            )}
-          </div>
-        </div>
-      </div>
-      <div className="waste-bin">
-        <div>
-          <label htmlFor="waste">Waste Type</label>
-          <div>
-            <select
-              name="waste"
-              id="waste"
-              ref={register({ required: "Please select a waste type." })}
-              className={`${errors.waste ? "ring-2 ring-red-500" : ""}`}
-              required
-            >
-              <option value="">Select Waste Type</option>
-              <option value="General Waste">General Waste</option>
-              <option value="Mixed Heavy">Mixed Heavy Waste</option>
-              <option value="Cleanfill/Hardfill">Cleanfill/Hardfill</option>
-              <option value="Green Garden Waste">Green Garden Waste</option>
-              <option value="Soil/Dirt">Soil/Dirt</option>
-            </select>
-            {errors.waste && errors.waste.message && (
-              <p>{errors.waste.message}</p>
-            )}
-          </div>
-        </div>
-        <div>
-          <label htmlFor="bin">Bin Size</label>
-          <div>
-            <select
-              name="bin"
-              id="bin"
-              ref={register({ required: "Please select a bin size." })}
-              className={`${errors.bin ? "ring-2 ring-red-500" : ""}`}
-              required
-            >
-              <option value="">Select Bin Size</option>
-              <option value="2m3">2 Cubic Meter Bin (2m3)</option>
-              <option value="3m3">3 Cubic Meter Bin (3m3)</option>
-              <option value="4m3">4 Cubic Meter Bin (4m3)</option>
-              <option value="5m3">5 Cubic Meter Bin (5m3)</option>
-              <option value="6m3">6 Cubic Meter Bin (6m3)</option>
-              <option value="8m3">8 Cubic Meter Bin (8m3)</option>
-              <option value="9m3">9 Cubic Meter Bin (9m3)</option>
-              <option value="10m3">10 Cubic Meter Bin (10m3)</option>
-            </select>
-            {errors.bin && errors.bin.message && <p>{errors.bin.message}</p>}
-          </div>
-        </div>
-      </div>
-      <div className="cs-dates">
-        <div>
-          <label htmlFor="delivery-date">Delivery Date</label>
-          <div>
-            <Controller
-              name="deliveryDate"
-              id="delivery-date"
-              control={control}
-              rules={{ required: "Please select a delivery date." }}
-              required
-              defaultValue=""
-              render={({ onChange, value }) => (
-                <ReactDatePicker
-                  onChange={onChange}
-                  selected={value}
-                  selectsStart
-                  minDate={new Date()}
-                  startDate={deliveryDate}
-                  endDate={returnDate}
-                  placeholderText="Choose delivery date"
-                  className={`${
-                    errors.deliveryDate ? "ring-2 ring-red-500" : ""
-                  }`}
+                {errors.name && errors.name.message && (
+                  <p>{errors.name.message}</p>
+                )}
+              </div>
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <div>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  className={`${errors.email ? "ring-2 ring-red-500" : ""}`}
+                  ref={register({
+                    required: "Email is required",
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                      message: "Invalid email address.",
+                    },
+                  })}
+                  required
                 />
-              )}
-            />
-            {errors.deliveryDate && errors.deliveryDate.message && (
-              <p>{errors.deliveryDate.message}</p>
-            )}
+                {errors.email && errors.email.message && (
+                  <p>{errors.email.message}</p>
+                )}
+              </div>
+            </div>
           </div>
-        </div>
-        <div>
-          <label htmlFor="return-date">Return Date</label>
-          <div>
-            <Controller
-              name="returnDate"
-              id="return-date"
-              control={control}
-              rules={{ required: "Please select a delivery return date." }}
-              required
-              defaultValue=""
-              render={({ onChange, value }) => (
-                <ReactDatePicker
-                  onChange={onChange}
-                  selected={value}
-                  selectsEnd
-                  minDate={deliveryDate}
-                  startDate={deliveryDate}
-                  endDate={returnDate}
-                  placeholderText="Choose delivery return date"
-                  className={`${
-                    errors.returnDate ? "ring-2 ring-red-500" : ""
-                  }`}
+          <div className="suburbs cs-form-control">
+            <div>
+              <label htmlFor="suburb">Suburb</label>
+              <div>
+                <Controller
+                  name="suburb"
+                  id="suburb"
+                  control={control}
+                  rules={{ required: "Please select your suburb." }}
+                  defaultValue=""
+                  render={({ onChange }) => (
+                    <ReactSelect
+                      onChange={onChange}
+                      options={options}
+                      isClearable
+                      isSearchable
+                      styles={customStyles}
+                      components={{ DropdownIndicator }}
+                      placeholder="Type your suburb name..."
+                      className={`${
+                        errors.suburb ? "ring-2 ring-red-500" : ""
+                      }`}
+                    />
+                  )}
                 />
-              )}
-            />
-            {errors.returnDate && errors.returnDate.message && (
-              <p>{errors.returnDate.message}</p>
-            )}
+                {errors.suburb && errors.suburb.message && (
+                  <p>{errors.suburb.message}</p>
+                )}
+              </div>
+            </div>
           </div>
+          <div className="waste-bin cs-form-control">
+            <div>
+              <label htmlFor="waste">Waste Type</label>
+              <div className="relative">
+                <select
+                  name="waste"
+                  id="waste"
+                  ref={register({ required: "Please select a waste type." })}
+                  className={`${errors.waste ? "ring-2 ring-red-500" : ""}`}
+                  required
+                >
+                  <option value="">Select Waste Type</option>
+                  <option value="General Waste">General Waste</option>
+                  <option value="Mixed Heavy">Mixed Heavy Waste</option>
+                  <option value="Cleanfill/Hardfill">Cleanfill/Hardfill</option>
+                  <option value="Green Garden Waste">Green Garden Waste</option>
+                  <option value="Soil/Dirt">Soil/Dirt</option>
+                </select>
+                <ChevronDown />
+                {errors.waste && errors.waste.message && (
+                  <p>{errors.waste.message}</p>
+                )}
+              </div>
+            </div>
+            <div>
+              <label htmlFor="bin">Bin Size</label>
+              <div className="relative">
+                <select
+                  name="bin"
+                  id="bin"
+                  ref={register({ required: "Please select a bin size." })}
+                  className={`${errors.bin ? "ring-2 ring-red-500" : ""}`}
+                  required
+                >
+                  <option value="">Select Bin Size</option>
+                  <option value="2m3">2 Cubic Meter Bin (2m3)</option>
+                  <option value="3m3">3 Cubic Meter Bin (3m3)</option>
+                  <option value="4m3">4 Cubic Meter Bin (4m3)</option>
+                  <option value="5m3">5 Cubic Meter Bin (5m3)</option>
+                  <option value="6m3">6 Cubic Meter Bin (6m3)</option>
+                  <option value="8m3">8 Cubic Meter Bin (8m3)</option>
+                  <option value="9m3">9 Cubic Meter Bin (9m3)</option>
+                  <option value="10m3">10 Cubic Meter Bin (10m3)</option>
+                </select>
+                <ChevronDown />
+                {errors.bin && errors.bin.message && (
+                  <p>{errors.bin.message}</p>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="cs-dates cs-form-control">
+            <div>
+              <label htmlFor="delivery-date">Delivery Date</label>
+              <div className="relative">
+                <Controller
+                  name="deliveryDate"
+                  id="delivery-date"
+                  control={control}
+                  rules={{ required: "Please select a delivery date." }}
+                  required
+                  defaultValue=""
+                  render={({ onChange, value }) => (
+                    <ReactDatePicker
+                      onChange={onChange}
+                      selected={value}
+                      selectsStart
+                      minDate={new Date()}
+                      startDate={deliveryDate}
+                      endDate={returnDate}
+                      placeholderText="Choose delivery date"
+                      className={`${
+                        errors.deliveryDate ? "ring-2 ring-red-500" : ""
+                      }`}
+                    />
+                  )}
+                />
+                <Calendar />
+                {errors.deliveryDate && errors.deliveryDate.message && (
+                  <p>{errors.deliveryDate.message}</p>
+                )}
+              </div>
+            </div>
+            <div>
+              <label htmlFor="return-date">Return Date</label>
+              <div className="relative">
+                <Controller
+                  name="returnDate"
+                  id="return-date"
+                  control={control}
+                  rules={{ required: "Please select a delivery return date." }}
+                  required
+                  defaultValue=""
+                  render={({ onChange, value }) => (
+                    <ReactDatePicker
+                      onChange={onChange}
+                      selected={value}
+                      selectsEnd
+                      minDate={deliveryDate}
+                      startDate={deliveryDate}
+                      endDate={returnDate}
+                      placeholderText="Choose delivery return date"
+                      className={`${
+                        errors.returnDate ? "ring-2 ring-red-500" : ""
+                      }`}
+                    />
+                  )}
+                />
+                <Calendar />
+                {errors.returnDate && errors.returnDate.message && (
+                  <p>{errors.returnDate.message}</p>
+                )}
+              </div>
+            </div>
+          </div>
+          <input
+            type="hidden"
+            name="subject"
+            ref={register}
+            defaultValue="Book a Bin"
+          />
+          <button
+            type="submit"
+            aria-label="Submit"
+            disabled={isSubmitting}
+            className="mt-8 cs-btn"
+          >
+            {isSubmitting ? "Submitting..." : "Submit"}
+          </button>
+          {isFormSubmitting && (
+            <div className="absolute bg-white bg-opacity-50 w-full h-full top-0 cs-spinner flex items-center justify-center">
+              <Loading />
+            </div>
+          )}
+        </form>
+      )}
+      {isFormSubmitted && (
+        <div className="max-w-500px mx-auto mt-8 text-center">
+          Thank you! We will get back to you shortly.
         </div>
-      </div>
-      <input
-        type="hidden"
-        name="subject"
-        ref={register}
-        defaultValue="Book a Bin"
-      />
-      <button type="submit" aria-label="Submit" disabled={isSubmitting}>
-        {isSubmitting ? "Submitting..." : "Submit"}
-      </button>
-    </form>
+      )}
+    </>
   )
 }
 
--- src/components/Home/HomeForm.js
+++ src/components/Home/HomeForm.js
@@ -4,7 +4,7 @@ import ContactForm5 from "../Forms/ContactForm5"
 
 const HomeForm = ({ bookNowTitle, bookNowContent }) => {
   return (
-    <section className="cs-home-form pt-10 pb-20 bg-gradient-to-b from-white to-light-green">
+    <section className="cs-home-form pt-10 pb-20 bg-gradient-to-b from-white to-light-green lg:py-32">
       <div className="px-15px text-center">
         <h3 className="text-h3-sm mb-4 lg:text-h3-lg">{bookNowTitle}</h3>
         <div dangerouslySetInnerHTML={{ __html: bookNowContent }} />
